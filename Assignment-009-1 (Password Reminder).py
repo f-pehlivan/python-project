408f1a80cb6384d50044ac4aa04bd4dd7d3d8803
f-pehlivan <1pehlivanfaruk@gmail.com>
2021-07-16 15:08:13 +0200

    Assignment completed

--- Assignment-009-1 (Password Reminder).py
+++ Assignment-009-1 (Password Reminder).py
@@ -1,1 +1,9 @@
+name = "Faruk"
+password = "W1q2a3@"
 
+given_name = str(input("Please enter your name :"))
+
+if given_name == name :
+    print("Hello, {}! The password is : {}".format(given_name, password))
+else:
+    print("Hello, {}! See you later.".format(given_name))
