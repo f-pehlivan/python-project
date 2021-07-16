4352ba248a4f3110b778f0586e9ce01092c5dd2d
f-pehlivan <1pehlivanfaruk@gmail.com>
2021-07-16 15:53:55 +0200

    Assignment completed

--- Assignment-009-2_(Covid-19_Risk).py
+++ Assignment-009-2_(Covid-19_Risk).py
@@ -1,1 +1,8 @@
-
+age = input("Are you a cigarette addict older than 75 years old? ('Yes' or 'No') : ").capitalize()
+chronic = input("Do you have a severe chronic disease? ('Yes' or 'No') : ").capitalize()
+immune = input("Is your immune system too weak? ('Yes' or 'No') : ").capitalize()
+risk = (age == "Yes") or (immune == "Yes") or (chronic == "Yes")
+if risk :
+    print("You are in risky group")
+else :
+    print("You are not in risky group")
